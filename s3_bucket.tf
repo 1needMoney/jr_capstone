17812e1a447eb8d85d3006034b67bbb522f3bb90
1needMoney <ineedmoney0322@gmail.com>
2024-12-18 11:26:43 +0100

    updated s3 bucket config

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -1,19 +1,29 @@
 # s3_bucket.tf
 
 resource "aws_s3_bucket" "wordpress_media" {
-  bucket = "wordpress-media-bucket-{{random_id.bucket_id.hex}}"
-  acl    = "private"  # Keep the bucket private
+  bucket = "wordpress-media-bucket-${random_id.bucket_id.hex}"
 
   tags = {
-    Name = "WordPressMediaBucket"
+    Name        = "WordPressMediaBucket"
     Environment = "Production"
   }
 }
 
-# Optionally, you can create a bucket policy to allow public access for WordPress media
-resource "aws_s3_bucket_object" "wordpress_media_index" {
-  bucket = aws_s3_bucket.wordpress_media.bucket
-  key    = "index.html"
+resource "aws_s3_bucket_acl" "wordpress_media_acl" {
+  bucket = aws_s3_bucket.wordpress_media.id
   acl    = "private"
+}
+
+
+# Optionally, you can create a bucket policy to allow public access for WordPress media
+resource "aws_s3_object" "wordpress_media_index" {
+  bucket  = aws_s3_bucket.wordpress_media.id
+  key     = "index.html"
   content = "<html><body><h1>WordPress Media Bucket</h1></body></html>"
+}
+
+resource "aws_s3_object_acl" "wordpress_media_index_acl" {
+  bucket = aws_s3_bucket.wordpress_media.id
+  key    = aws_s3_object.wordpress_media_index.key
+  acl    = "private"
 }
